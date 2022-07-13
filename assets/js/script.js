817e31a39b05810824c1392dfee074eb9937c0ed
smandla <35283559+smandla@users.noreply.github.com>
2022-07-13 11:49:37 -0700

    filter recipes

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,13 +83,105 @@ var dishType = [
   "Starter",
   "Sweets",
 ];
+var search = "";
+var dietFilter = "";
+var cuisineFilter = "";
+var healthFilter = "";
+var mealTypeFilter = "";
+
 function init() {
-  getData();
+  showFilterData();
+  //   getData();
 }
+var dietEl = $("#diet");
+function showFilterData() {
+  showDietFilter();
+  showHealthFilter();
+  showCuisineFilter();
+  showMealTypeFilter();
+}
+// var aEl = $("<a>");
+function showDietFilter() {
+  //   var dietDropdown = $(``);
+  //<p>You can insert <strong>any type of content</strong> within the dropdown menu.</p>
+  var dietDropdownItems = $("#diet_dropdown_content");
+  for (let i = 0; i < dietArray.length; i++) {
+    // console.log(dietArray[i]);
+    var dietPEl = $("<a>")
+      .addClass("dropdown-item")
+      .attr("id", dietArray[i])
+      .text(dietArray[i]);
+
+    dietPEl.on("click", function (e) {
+      dietFilter = e.target.innerHTML;
+      console.log(e.target.innerHTML);
+    });
+    dietPEl.appendTo(dietDropdownItems);
+  }
+  //   dietDropdown.appendTo(dietEl);
+}
+function showHealthFilter() {
+  //   var dietDropdown = $(``);
+  //<p>You can insert <strong>any type of content</strong> within the dropdown menu.</p>
+  var healthDropdownItems = $("#health_dropdown_content");
+  for (let i = 0; i < healthArray.length; i++) {
+    // console.log(healthArray[i]);
+    var healthPEl = $("<a>")
+      .addClass("dropdown-item")
+      .attr("id", healthArray[i])
+      .text(healthArray[i]);
+    healthPEl.on("click", function (e) {
+      healthFilter = e.target.innerHTML;
+      console.log(e.target.innerHTML);
+    });
+    healthPEl.appendTo(healthDropdownItems);
+  }
+  // dietDropdown.appendTo(dietEl);
+}
+function showMealTypeFilter() {
+  //   var dietDropdown = $(``);
+  //<p>You can insert <strong>any type of content</strong> within the dropdown menu.</p>
+  var mealTypeDropdownItems = $("#meal");
+  for (let i = 0; i < mealType.length; i++) {
+    // console.log(mealType[i]);
+    var mealTypePEl = $("<button>")
+      .addClass("button")
+      .attr("id", mealType[i])
+      .text(mealType[i]);
+    mealTypePEl.on("click", function (e) {
+      mealTypeFilter = e.target.innerHTML;
+      console.log(e.target.innerHTML);
+    });
+    mealTypePEl.appendTo(mealTypeDropdownItems);
+  }
+  //   dietDropdown.appendTo(dietEl);
+}
+function showCuisineFilter() {
+  //   var dietDropdown = $(``);
+  //<p>You can insert <strong>any type of content</strong> within the dropdown menu.</p>
+  var cuisineDropdownItems = $("#cuisine_dropdown_content");
+  for (let i = 0; i < cuisineTypeArray.length; i++) {
+    // console.log(cuisine[i]);
+    var cuisinePEl = $("<a>")
+      .addClass("dropdown-item")
+      .attr("id", cuisineTypeArray[i])
+      .text(cuisineTypeArray[i]);
+    cuisinePEl.on("click", function (e) {
+      cuisineFilter = e.target.innerHTML;
+      $("#cuisineVal").html();
+      cuisinePEl.val(cuisineFilter);
+      console.log(e.target.innerHTML);
+    });
+    cuisinePEl.appendTo(cuisineDropdownItems);
+  }
+  //   dietDropdown.appendTo(dietEl);
+}
+
 const getData = async () => {
   var response = await fetch(
-    "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=c80dede8&app_key=b554f0e254b1650c2d9edaa1fe10ca4d"
+    `https://api.edamam.com/api/recipes/v2?type=public&q=${search}&app_id=c80dede8&app_key=b554f0e254b1650c2d9edaa1fe10ca4d&diet=${dietFilter}&health=${healthFilter}&cuisineType=${cuisineFilter}&mealType=${mealTypeFilter}`
   );
+
   var data = await response.json();
   var recipes = data.hits;
   showData(recipes);
@@ -113,7 +205,20 @@ function showData(recipes) {
     cardEl.appendTo(cardContainerEl);
   }
 }
+var formEl = $("#form");
+var searchInputEl = $("#search_input");
 
+var searchBtnEl = $("#search_btn");
+searchBtnEl.on("click", function (e) {
+  e.preventDefault();
+  search = searchInputEl.val();
+  console.log(search, dietFilter, cuisineFilter, healthFilter, mealTypeFilter);
+
+  getData();
+
+  //   console.log(searchInputEl.val());
+});
+// bulmaSlider.attach();
 /**
  * <div class="card_container">
                 <div class="card g" >
